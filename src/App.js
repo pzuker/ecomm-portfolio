df0c0345c33cfe6c3adc62e53bc74425ab217739
Pablo Gabriel Zukerman <zukermanpablo@gmail.com>
2023-06-04 19:11:07 -0600

    add sign in component with firebase db and libraries

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/signin/signin.component';
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-      route: 'shop/hats',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-      route: 'shop/jackets',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-      route: 'shop/sneakers',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-      route: 'shop/womens',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-      route: 'shop/mens',
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="signin" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,52 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB5u1_3En194Si167-zIXcXHKBt3CamNkY',
+  authDomain: 'ecomm-portfolio-db.firebaseapp.com',
+  projectId: 'ecomm-portfolio-db',
+  storageBucket: 'ecomm-portfolio-db.appspot.com',
+  messagingSenderId: '272205116400',
+  appId: '1:272205116400:web:0c28077bb214b518a12335',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt: new Date(),
+      });
+    } catch (err) {
+      console.log('error creating the user', err.mesage);
+    }
+  }
+
+  return userDocRef;
+};
